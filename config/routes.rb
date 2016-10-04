26d459e3f3939ec18816ecfc710168a2f5c151f3
khung9895 <khung9895@gmail.com>
2016-10-05 01:22:13 +0700

    basic model user and secure password

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
   root 'static_pages#home'
+  get  'help'    => 'static_pages#help'
+  get  'signup'	  => 'user#new'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
