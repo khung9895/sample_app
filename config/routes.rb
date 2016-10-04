f5d4c185e01ef9781d113fd63258fe3d30921c1a
khung9895 <khung9895@gmail.com>
2016-10-05 02:33:18 +0700

    include basic sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   get  'help'    => 'static_pages#help'
-  get  'signup'	  => 'user#new'
+  get  'signup'	 => 'users#new'
+  post 'signup'  => 'users#create'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
